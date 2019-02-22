d3e8a51416c4eb42eb089fed9294253a436e8f29
AdamMarcus <adam.marcus.austin@gmail.com>
2019-02-22 11:54:50 -0700

    Established tool subclasses. Removed tool price method.

--- src/tool.py
+++ src/tool.py
@@ -11,49 +11,34 @@ class Tool:
         self.name = tool_name
         self.daysTillReturn = 0
         self.pricePerDay = SetToolPrice.getPrice(self.name)
-        return 1
-
-    def returnTool(self):
-        return -1
-        
-
-class SetToolPrice:
-
-    def getPrice(tool):
-        if 'concrete' in tool:
-            return 3.0
-        if "painting" in tool:
-            return 1.0
-        if "plumbing" in tool:
-            return 2.0
-        if "woodwork" in tool:
-            return 2.0
-        if "yardwork" in tool:
-            return 3.0
-
-class ConcreteTool(Tool):
 
     def returnTool(self):
         return -1
 
 class PaintingTool(Tool):
 
-    def returnTool(self):
-        return -1
+    def __init__(self, tool_name):
+        self.name = tool_name
+        self.pricePerDay = 1.0
+
+class ConcreteTool(Tool):
+    def __init__(self, tool_name):
+        self.name = tool_name
+        self.pricePerDay = 3.0
+
 
 class PlumbingTool(Tool):
-
-    def returnTool(self):
-        return -1
+    def __init__(self, tool_name):
+        self.name = tool_name
+        self.pricePerDay = 2.0
 
 
 class WoodworkTool(Tool):
-
-    def returnTool(self):
-        return -1
-
+    def __init__(self, tool_name):
+        self.name = tool_name
+        self.pricePerDay = 2.0
 
 class YardworkTool(Tool):
-
-    def returnTool(self):
-        return -1
+    def __init__(self, tool_name):
+        self.name = tool_name
+        self.pricePerDay = 1.0

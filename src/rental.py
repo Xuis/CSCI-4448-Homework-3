a6c682bbd3b1f21fa0d47899a1c8a071207022c7
Will Harris <Xuis@users.noreply.github.com>
2019-02-24 17:12:43 -0700

    added getDueDate() and reportString

--- src/rental.py
+++ src/rental.py
@@ -16,14 +16,25 @@ Take in list of tools and record rental date and must return date
 """
 
 class Rental:
-
-    def __init__(self, customer, date, tools, length):
+    def __init__(self, customer, date, tools, daysRented):
         self.customer = customer
         self.date = date #TODO Get the current Date automatically
         self.tools = tools
-        self.length = length
+        self.days = daysRented
 
         # Calculate the cost of the rental:
         self.cost = 0
         for tool in tools:
-            self.cost += tool.getCostPerDay() * length
+            self.cost += tool.getCostPerDay() * self.days
+
+    def getDueDate(self):
+        return self.date + self.days
+
+    def reportString(self):
+        # Retuns a "report" of the rental in a string formself.
+        # for use in print() statements
+        toolsString = ""
+        for tool in tools:
+            toolsString += (tool.getName()  + " ")
+        string = customer.name + " rented [" + toolsString + "] for " + self.days + " days which totaled to $" +  self.cost
+        return string

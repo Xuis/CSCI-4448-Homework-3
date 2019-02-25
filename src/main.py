4b6719650233a039bd642d334dad8ebe090f00ee
AdamMarcus <adam.marcus.austin@gmail.com>
2019-02-25 10:25:10 -0700

    Made simulation work for 35 days.

--- src/main.py
+++ src/main.py
@@ -2,23 +2,29 @@
 
 # =============================================================================
 
-from store import Store
+# from store import Store
 # from src.tool import Tool
+from storeDate import StoreDate
 
 NUM_SIMULATION_DAYS = 35
 
 print ("The program runs!")
 
-hardwareStore = Store()
+# hardwareStore = Store()
 
 # Each day:
 for day in range(0, NUM_SIMULATION_DAYS):
     print("Day {} simulated.".format(day))
-    hardwareStore.open(day)
-    hardwareStore.close()
+    # hardwareStore.open(day)
+    # hardwareStore.close()
     # Shuffle CustomerList
 
-    # While (isDay)
+    currSim = StoreDate()
+    today = currSim.simulateDay()
+    while (today != -1):
+        print(today)
+        today = currSim.simulateDay()
+
 
     # If tools are available
         # Customer enters stor
--- src/storeDate.py
+++ src/storeDate.py
@@ -4,13 +4,16 @@
 
 class StoreDate:
     # World duration will be the duration of the simulation
-	def __init__(self):
-		self.todayInd = 0
+    def __init__(self):
+        self.todayInd = 1
         self.worldDuration = 35
-    
-    def __init__(self, worldDuration):
-		self.todayInd = 0
-        self.worldDuration = worldDuration
 
     def getDay(self):
         return todayInd
+
+    def simulateDay(self):
+        currDay = self.todayInd
+        if currDay > self.worldDuration:
+            return -1
+        self.todayInd += 1
+        return currDay

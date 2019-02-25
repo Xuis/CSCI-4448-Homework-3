ea667aebb198ec3e9d78261322b37e26baaeb79e
JadeWibbels <erin.wibbels@colorado.edu>
2019-02-25 10:47:06 -0700

    customer list fills itself at init, tools catalog

--- src/store.py
+++ src/store.py
@@ -7,7 +7,7 @@
 
 class Store:
     def __init__(self, catalog, client):
-        self.customerList = client.List
+        self.customerList = client.list
         self.inventory = catalog
         self.month = [i for i in range(35)]
         self.rentals = {}
@@ -26,4 +26,3 @@ class Store:
                              "Payment": rentalTotal,
                              "Day Rented": dayRented,
                              "Due Date": dayDue}
-		

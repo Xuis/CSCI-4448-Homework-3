06eefb06f68b5c5659c1ad2f37162ba4dae9819b
JadeWibbels <erin.wibbels@colorado.edu>
2019-02-26 10:34:16 -0700

    tool factory in place. Removed duplicated code in tools

--- src/catalog.py
+++ src/catalog.py
@@ -2,9 +2,31 @@
 Catalog to contain the 20 tool offerings that the store will rent
 """
 
-from tool import ConcreteTool, PaintingTool, PlumbingTool, WoodWorkTool, YardWorkTool
+from tool import ToolFactory
 
 class Catalog:
+    def __init__(self):
+        self.onhand = []
+        self.create_catalog()
+        self.onloan = []
+
+    def create_catalog(self):
+        for i in range(4):
+            self.onhand.append(ToolFactory.create_tool("concrete_{}".format(i)))
+            self.onhand.append(ToolFactory.create_tool("painting_{}".format(i)))
+            self.onhand.append(ToolFactory.create_tool("plumbing_{}".format(i)))
+            self.onhand.append(ToolFactory.create_tool("woodwoork_{}".format(i)))
+            self.onhand.append(ToolFactory.create_tool("yardwork_{}".format(i)))
+        return self.onhand
+
+    def getToolCost(self, tool):
+        return tool.getCostPerDay()
+
+    def getToolAvail(self, tool):
+        return tool.isRented()
+
+    def getOnhand(self):
+        return self.onhand
     def rentTool(self, tools):
         # move tool from on hand to rented
         for tool in tools:
@@ -23,27 +45,4 @@ class Catalog:
             self.onhand.append(tool)
 
 
-# -------------------- New UML Methods -------------------
-    def __init__(self):
-        self.onhand = []
-        self.create_catalog()
-        self.onloan = []
-        self.create_catalog
 
-    def create_catalog(self):
-        for i in range(4):
-            self.onhand.append(ConcreteTool("concrete_{}".format(i)))
-            self.onhand.append(PaintingTool("painting_{}".format(i)))
-            self.onhand.append(PlumbingTool("plumbing_{}".format(i)))
-            self.onhand.append(WoodWorkTool("woodwoork_{}".format(i)))
-            self.onhand.append(YardWorkTool("yardwork_{}".format(i)))
-        return self.onhand
-
-    def getToolCost(self, tool):
-        return tool.getCostPerDay()
-
-    def getToolAvail(self, tool):
-        return tool.isRented()
-
-    def getOnhand(self):
-        return self.onhand
